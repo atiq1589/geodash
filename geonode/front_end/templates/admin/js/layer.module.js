5e27d04ab024b1db9bd1cd7dead0c3e31f8322ab
Rashedul Alam <rashedulalam.iit.du@gmail.com>
2018-01-17 19:39:42 +0600

    layer approval from based on demo server side json

---
+++ geonode/front_end/templates/admin/js/layer.module.js
@@ -0,0 +1,8 @@
+(function(){
+    angular.module('layerApp', ['ngCookies','ui.grid', 'ui.grid.selection', 'ui.grid.cellNav','ui.grid.autoResize'])
+    .config(['$interpolateProvider', '$locationProvider', function($interpolateProvider, $locationProvider) {
+        $interpolateProvider.startSymbol('[{');
+        $interpolateProvider.endSymbol('}]');
+        $locationProvider.html5Mode(true);
+    }]);
+})();
