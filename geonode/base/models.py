1fa70cf9f724b40710b1b92a0a98c1d89d04cc51
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2018-05-22 15:44:35 +0600

    fix login timeout problem

--- geonode/base/models.py
+++ geonode/base/models.py
@@ -1002,7 +1002,7 @@ def do_login(sender, user, request, **kwargs):
             token = u.hex
 
         # Do GeoServer Login
-        url = "%s%s?access_token=%s" % (settings.OGC_SERVER['default']['PUBLIC_LOCATION'],
+        url = "%s%s?access_token=%s" % (settings.OGC_SERVER['default']['LOCATION'],
                                         'ows?service=wms&version=1.3.0&request=GetCapabilities',
                                         token)
 
@@ -1050,14 +1050,14 @@ def do_logout(sender, user, request, **kwargs):
             access_token = None
 
         if access_token:
-            url = "%s%s?access_token=%s" % (settings.OGC_SERVER['default']['PUBLIC_LOCATION'],
+            url = "%s%s?access_token=%s" % (settings.OGC_SERVER['default']['LOCATION'],
                                             settings.OGC_SERVER['default']['LOGOUT_ENDPOINT'],
                                             access_token)
             header_params = {
                 "Authorization": ("Bearer %s" % access_token)
             }
         else:
-            url = "%s%s" % (settings.OGC_SERVER['default']['PUBLIC_LOCATION'],
+            url = "%s%s" % (settings.OGC_SERVER['default']['LOCATION'],
                             settings.OGC_SERVER['default']['LOGOUT_ENDPOINT'])
 
         param = {}
